608ce94dfada8201f25150d986aa8ec9e50eb02d
GuilhE <gui.delgado@hotmail.com>
2020-09-11 16:52:05 +0100

    removed pom dependency gen

--- deploy-bintray.gradle.kts
+++ deploy-bintray.gradle.kts
@@ -112,6 +112,7 @@ configure<PublishingExtension> {
 //                    val dependenciesNode = asNode().appendNode("dependencies")
 //                    configurations.getByName("lintPublish") {
 //                        dependencies.forEach {
+//                            println(it)
 //                            val dependencyNode = dependenciesNode.appendNode("dependency")
 //                            dependencyNode.appendNode("groupId", it.group)
 //                            dependencyNode.appendNode("artifactId", it.name)
